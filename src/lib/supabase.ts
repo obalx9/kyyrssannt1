e5f0baf8ee2224048f8650ab8dc5e29ebd56deff
obalx9 <globalx9@gmail.com>
2026-02-24 14:30:33 +0300

    Delete src/lib/supabase.ts

--- src/lib/supabase.ts
+++
@@ -1,5 +0,0 @@
-// This file is kept for backwards compatibility
-// All types have been moved to types.ts
-// Supabase client has been removed - using custom API instead
-
-export * from './types';
